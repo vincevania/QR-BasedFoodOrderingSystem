2c5d25c9c8561ac32fc84cecd159cbafccaefdc2
vincevania <raiabhishek314@gmail.com>
2024-02-16 18:26:16 +0530

    test with branches

--- YMUM/src/Components/AdminInterface/QuickInsight.jsx
+++ YMUM/src/Components/AdminInterface/QuickInsight.jsx
@@ -7,7 +7,7 @@ function QuickInsight() {
             <div className='flex flex-shrink justify-between'>
               <span className="block text-gray-500 text-base">Total Items
                 <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" className="bi bi-box" viewBox="0 0 16 16" style={{marginLeft:'0.5rem'}}>
-                <path d="M8.186 1.113a.5.5 0 0 0-.372 0L1.846 3.5 8 5.961 14.154 3.5zM15 4.239l-6.5 2.6v7.922l6.5-2.6V4.24zM7.5 14.762V6.838L1 4.239v7.923zM7.443.184a1.5 1.5 0 0 1 1.114 0l7.129 2.852A.5.5 0 0 1 16 3.5v8.662a1 1 0 0 1-.629.928l-7.185 2.874a.5.5 0 0 1-.372 0L.63 13.09a1 1 0 0 1-.63-.928V3.5a.5.5 0 0 1 .314-.464z"/>
+               <path d="M8.186 1.113a.5.5 0 0 0-.372 0L1.846 3.5 8 5.961 14.154 3.5zM15 4.239l-6.5 2.6v7.922l6.5-2.6V4.24zM7.5 14.762V6.838L1 4.239v7.923zM7.443.184a1.5 1.5 0 0 1 1.114 0l7.129 2.852A.5.5 0 0 1 16 3.5v8.662a1 1 0 0 1-.629.928l-7.185 2.874a.5.5 0 0 1-.372 0L.63 13.09a1 1 0 0 1-.63-.928V3.5a.5.5 0 0 1 .314-.464z"/>
                 </svg>
               </span>
               <span className="block text-3xl font-bold">50</span>
